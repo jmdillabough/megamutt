082d961abbf9000042a0f77879ea1b23f5c98de2
Jonathan Dillabough <jdillabough312@protonmail.com>
2023-03-03 07:39:51 -0600

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Navbar from '../components/Navbar'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" data-theme='cyberpunk'>
       <Head />
       <body>
+        <Navbar />
         <Main />
         <NextScript />
       </body>
